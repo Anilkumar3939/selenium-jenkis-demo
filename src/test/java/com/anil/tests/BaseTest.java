8f2d4a89f174514913ec9b58e5aee9902a150378
Anil Kumar <anilkumar3939104@gmail.com>
2026-07-27 01:20:56 +0530

    changed base test

--- src/test/java/com/anil/tests/BaseTest.java
+++ src/test/java/com/anil/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package com.anil.tests;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -8,7 +9,12 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup(){
-        driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+
+    driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
