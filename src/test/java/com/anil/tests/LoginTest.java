70758e538d31fe975c03f7fd3d00da4821c8598b
Anil Kumar <anilkumar3939104@gmail.com>
2026-07-27 16:08:44 +0530

    added allure

--- src/test/java/com/anil/tests/LoginTest.java
+++ src/test/java/com/anil/tests/LoginTest.java
@@ -2,22 +2,36 @@ package com.anil.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import com.anil.pages.LoginPage;
 
-public class LoginTest extends BaseTest{
-    
-    @Test
-    public void validLogin(){
+import io.qameta.allure.*;
+
+@Epic("OrangeHRM")
+@Feature("Login")
+public class LoginTest extends BaseTest {
+
+    @Test(description = "Verify valid login")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Verify user can login with valid username and password")
+    public void validLogin() {
+
         LoginPage loginPage = new LoginPage(driver);
+
         loginPage.login("Admin", "admin123");
+
         Assert.assertTrue(loginPage.validCredentials());
     }
 
-    @Test
-    public void inValidLogin(){
+    @Test(description = "Verify invalid login")
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify error message is displayed for invalid credentials")
+    public void inValidLogin() {
+
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("anil","1234");
+
+        loginPage.login("anil", "1234");
+
         Assert.assertTrue(loginPage.invalidCredentials());
     }
-    
 }
